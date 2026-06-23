e4d35d9cf37288f552d037c4ef8fa5cdbb09c96c
Mifa43 <mifa43kotez@gmail.com>
2026-06-23 21:56:02 +0200

    Dodat poziv SupportedExtension u detekciji fajla

--- genomatic/program/parser/helpers/detect_file_type.py
+++ genomatic/program/parser/helpers/detect_file_type.py
@@ -1,20 +1,17 @@
 from pathlib import Path
 
+from models.supported_extensions import SupportedExtension
+
 PROJECT_ROOT = Path(__file__).resolve().parents[4]
 GENOMATIC_ROOT = PROJECT_ROOT / "genomatic"
 
 SAMPLE_WORKING_DIR = GENOMATIC_ROOT / "genomatic1000_raw_input_samples"
 
-SUPPORTED_EXTENSIONS = {
-    ".csv",
-    ".tsv",
-    ".json",
-    ".txt",
-    ".vcf",
-    ".bed",
-}
-
-
 def detect_file_type(file_path: str | Path) -> str | None:
+    
     file_extension = Path(file_path).suffix.lower()
-    return file_extension if file_extension in SUPPORTED_EXTENSIONS else None
+    
+    try:
+        return file_extension if file_extension in SupportedExtension(file_extension).value else None
+    except ValueError:
+        return None
